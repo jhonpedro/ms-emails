b4f4998638782ea2d5fe0ac18a1670838b9d8c7f
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-29 16:08:35 -0300

    feat: add two new consumers

--- src/kafka/consumers/index.js
+++ src/kafka/consumers/index.js
@@ -1,9 +1,14 @@
 const adminMailConsumer = require('./adminMail')
+const newClientMail = require('./newClientMail')
+const newForgotPassword = require('./newForgotPassword')
 
 async function rootConsumer() {
 	try {
 		await adminMailConsumer()
+		await newClientMail()
+		await newForgotPassword()
 	} catch (error) {
+		console.error(error)
 		console.log('Error in consumer initialization')
 	}
 }
