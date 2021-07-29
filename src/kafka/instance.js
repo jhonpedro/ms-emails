7bf7ec7c8d2f1aa5433b3fc3189ebc2f3d9fe8de
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-29 15:59:49 -0300

    chore: change retry time and add env to broker url

--- src/kafka/instance.js
+++ src/kafka/instance.js
@@ -2,10 +2,13 @@ const { Kafka, logLevel } = require('kafkajs')
 
 const kafka = new Kafka({
 	clientId: 'ms-service',
-	brokers: ['localhost:9092'],
+	brokers: [
+		`${process.env.KAFKA_HOST}:9092`,
+		`${process.env.KAFKA_HOST}:29092`,
+	],
 	logLevel: logLevel.INFO,
 	retry: {
-		initialRetryTime: 300,
+		initialRetryTime: 2000,
 		retries: 10,
 	},
 })
